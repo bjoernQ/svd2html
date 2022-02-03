9a5159183eb28cc15df1e6c708b0099aab2775b2
Jesse Braham <jesse@beta7.io>
2022-02-03 15:36:39 -0800

    Complete rewrite, separate out peripheral pages and use templates

--- src/main.rs
+++ src/main.rs
@@ -1,245 +1,282 @@
-use std::collections::HashMap;
-use std::io::Write;
-use std::{fs::File, io::Read};
-use clap::StructOpt;
-use clap_derive::Parser;
+use std::{
+    collections::HashMap,
+    fs::{self, File},
+    io::Write,
+    path::PathBuf,
+};
 
-#[derive(Parser, Debug)]
-struct Args {
-    #[clap(short, long)]
-    input: String,
+use anyhow::Result;
+use clap::Parser;
+use lazy_static::lazy_static;
+use minijinja::{context, value::Value, Environment, Source, State};
+use svd_parser::svd::{Access, MaybeArray, PeripheralInfo, RegisterInfo};
 
-    #[clap(short, long)]
-    output: String,
+lazy_static! {
+    static ref ENV: Environment<'static> = create_environment();
 }
 
-fn main() {
-    static EMPTY: String = String::new();
+#[derive(Parser, Debug)]
+#[clap(version)]
+struct Opts {
+    /// SVD file to parse
+    #[clap(short, long)]
+    input: PathBuf,
 
-    let args = Args::parse();
+    /// Directory to write generated HTML files
+    #[clap(short, long, default_value = "output")]
+    output: PathBuf,
+}
 
-    let xml = &mut String::new();
-    File::open(args.input)
-        .unwrap()
-        .read_to_string(xml)
-        .expect("Unable to load SVD");
-    let svd = svd_parser::parse(xml).unwrap();
+fn main() -> Result<()> {
+    let opts = Opts::parse();
 
-    let mut writer = File::create(args.output).unwrap();
-
-    writeln!(
-        writer,
-        r#"<html><body>
-        <style type="text/css">
-        body {{
-            font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
-            margin: 16;
-        }}
-
-        h1 {{
-            background: white;
-            position: sticky;
-            top: 0;
-        }}
-
-        table, td {{
-            width: 100%;
-            border-collapse: collapse;
-        }}
-
-        table td {{
-            width: 1%; 
-            border: 1px solid black; 
-            text-align: center;
-        }}
-
-        table td.header {{
-            writing-mode: vertical-lr; 
-            transform: rotate(180deg) translate(0px, 8px);
-            width: 1%; border: none;
-            text-align: start;
-        }}
-
-        a {{
-            font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
-            margin: 16;
-            color: black;
-        }}
-    </style>
-    "#
-    )
-    .ok();
-
-    writeln!(
-        writer,
-        "<h1>Peripheral List</h1>",
-    )
-    .ok();
-
-    for (i,peripheral) in svd.peripherals.iter().enumerate() {
-        writeln!(
-            writer,
-            "<p><a href=\"#p{}\">{}</a></p>",
-            i, peripheral.name
-        )
-        .ok();
+    // Create the output directory if it does not already exist.
+    if !opts.output.exists() {
+        fs::create_dir_all(&opts.output)?;
     }
 
-    writeln!(
-        writer,
-        "</br></br>",
-    )
-    .ok();
+    // Parse the 'input' file, which should be an SVD (aka XML).
+    let xml = fs::read_to_string(&opts.input)?;
+    let svd = svd_parser::parse(&xml).unwrap();
 
+    // Convert the Vector of `MaybeArray<PeirpheralInfo>` to a Vector of just
+    // `PeripheralInfo`.
+    let peripherals = svd
+        .peripherals
+        .iter()
+        .filter_map(|p| match p {
+            MaybeArray::Single(pi) => Some(pi),
+            MaybeArray::Array(..) => unreachable!(), // Is it, though? ;)
+        })
+        .collect::<Vec<_>>();
 
-    for (i,peripheral) in svd.peripherals.iter().enumerate() {
-        writeln!(
-            writer,
-            "<h1 id=\"p{}\">{} (Base 0x{:8x})</h1>",
-            i, peripheral.name, peripheral.base_address
-        )
-        .ok();
-        writeln!(
-            writer,
-            "<p>{}</p>",
-            peripheral.description.as_ref().unwrap_or(&EMPTY)
-        )
-        .ok();
+    // Render each peripheral page. List each interupt and register, as well as each
+    // register's fields.
+    let chip = svd.name.clone();
+    for peripheral in &peripherals {
+        let filename = format!("{}.html", peripheral.name);
+        let html = render_peripheral(&chip, peripheral)?;
+        write_html(&html, &opts.output.join(filename))?;
+    }
 
-        if peripheral.interrupt.len() > 0 {
-            writeln!(writer, "<h2>Peripheral Interrupts</h2>").ok();
-            for interrupt in peripheral.interrupt.iter() {
-                let desc = match &interrupt.description {
-                    Some(desc) => desc.clone(),
-                    None => EMPTY.clone(),
-                };
-    
-                writeln!(
-                    writer,
-                    r#"<p><b>{}</b> <i>{}</i> {}</p>"#,
-                    interrupt.name, interrupt.value, desc
-                )
-                .ok();
+    // Render the index page, which lists all peripherals for a device with links to
+    // each peripheral's page.
+    let html = render_index(&chip, &peripherals)?;
+    write_html(&html, &opts.output.join("index.html"))?;
+
+    Ok(())
+}
+
+fn create_environment() -> Environment<'static> {
+    use minijinja::{Error, ErrorKind};
+
+    let mut env = Environment::new();
+    let mut src = Source::new();
+
+    src.load_from_path("templates", &["html"]).unwrap();
+    env.set_source(src);
+
+    // Define a custom function which allows us to include static files within our
+    // templates.
+    fn include_file(_state: &State, name: String) -> std::result::Result<String, Error> {
+        fs::read_to_string(&name).map_err(|e| {
+            Error::new(ErrorKind::ImpossibleOperation, "cannot load file").with_source(e)
+        })
+    }
+    env.add_function("include_file", include_file);
+
+    env
+}
+
+fn render_index(chip: &String, peripherals: &Vec<&PeripheralInfo>) -> Result<String> {
+    // Iterate through all peripherals, and constructor a Vector of Context
+    // containing the name and description for each.
+    let peripheral_info = peripherals
+        .iter()
+        .map(|p| {
+            context! {
+                name        => p.name.clone(),
+                description => p.description.clone().unwrap_or_default(),
             }
-            writeln!(writer, "</br>").ok();
-        }
+        })
+        .collect::<Vec<_>>();
 
-        for register in peripheral.registers() {
-            let sz = match register {
-                svd_parser::svd::MaybeArray::Single(_) => EMPTY.clone(),
-                svd_parser::svd::MaybeArray::Array(_, dim) => {
-                    format!("{}", dim.dim)
+    // Build the template context.
+    let ctx = context! {
+        chip        => chip,
+        peripherals => peripheral_info,
+    };
+
+    // Render the template to HTML using the context defined above.
+    let tmpl = ENV.get_template("index.html")?;
+    let html = tmpl.render(ctx)?;
+
+    Ok(html)
+}
+
+fn render_peripheral(chip: &String, peripheral: &PeripheralInfo) -> Result<String> {
+    // Build the template context.
+    let ctx = context! {
+        chip        => chip,
+        peripheral  => peripheral.name.clone(),
+        description => if let Some(desc) = &peripheral.description {
+            desc.to_owned()
+        } else {
+            String::new()
+        },
+        interrupts  => interrupts(peripheral),
+        registers   => registers(peripheral),
+    };
+
+    // Render the template to HTML using the context defined above.
+    let tmpl = ENV.get_template("peripheral.html")?;
+    let html = tmpl.render(ctx)?;
+
+    Ok(html)
+}
+
+fn interrupts(peripheral: &PeripheralInfo) -> Vec<Value> {
+    peripheral
+        .interrupt
+        .iter()
+        .map(|i| {
+            context! {
+                name        => i.name.clone(),
+                value       => i.value.to_string(),
+                description => if let Some(desc) = &i.description {
+                    desc.to_owned()
+                } else {
+                    String::new()
                 }
+            }
+        })
+        .collect::<Vec<_>>()
+}
+
+fn registers(peripheral: &PeripheralInfo) -> Vec<Value> {
+    peripheral
+        .registers()
+        .map(|register| {
+            let (ri, size) = match register {
+                MaybeArray::Single(ri) => (ri, 0u32),
+                MaybeArray::Array(ri, de) => (ri, de.dim),
             };
-            let name = register.name.replace("%s", &format!("<0..{}>", sz));
 
-            writeln!(
-                writer,
-                "<h2>{} (Offset 0x{:04x} Absolut 0x{:8x})</h2>",
-                name,
-                register.address_offset,
-                peripheral.base_address + register.address_offset as u64
-            )
-            .ok();
-            writeln!(
-                writer,
-                "<p>{}</p>",
-                register.description.as_ref().unwrap_or(&EMPTY)
-            )
-            .ok();
+            let absolute = peripheral.base_address + ri.address_offset as u64;
 
-            // show bits in table
-            writeln!(writer, r#"<table>"#).ok();
-
-            // field names
-            let flds: HashMap<_, _> = register
-                .fields()
-                .map(|f| (f.bit_offset() + f.bit_width() / 2, f))
-                .collect();
-            writeln!(writer, "<tr>").ok();
-            for bit in (0..32).rev() {
-                let text = if let Some(field) = flds.get(&bit) {
-                    &field.name
+            context! {
+                name        => ri.name.replace("%s", &format!("<0..{size}>")),
+                description => if let Some(desc) = &ri.description {
+                    desc.to_owned()
                 } else {
-                    &EMPTY
-                };
-                writeln!(writer, r#"<td class="header">{}</td>"#, text).ok();
+                    String::new()
+                },
+                offset      => format!("0x{:04x}", ri.address_offset),
+                absolute    => format!("0x{:08x}", absolute),
+                fields      => context! {
+                    names        => field_names(register),
+                    spans        => field_spans(register),
+                    descriptions => field_descriptions(register),
+                },
             }
-            writeln!(writer, "</tr>").ok();
+        })
+        .collect::<Vec<_>>()
+}
 
-            // field bit ranges (from > to)
-            let mut flds: Vec<(_, _)> = register
-                .fields()
-                .map(|f| (f.bit_offset() + f.bit_width() - 1, f.bit_offset()))
-                .rev()
-                .collect();
-            let mut at = 0;
-            for i in (0..flds.len()).rev() {
-                let (from, to) = flds[i];
-                if to > at {
-                    flds.insert(i + 1, (at + (to - at) - 1, at));
-                    at = from + 1;
-                } else {
-                    at = from + 1;
-                }
-            }
-            if flds.len() > 0 {
-                let (f, _) = flds[0];
-                if f < 31 {
-                    flds.insert(0, (31, f + 1));
-                }
+fn field_names(register: &MaybeArray<RegisterInfo>) -> Vec<String> {
+    let fields = register
+        .fields()
+        .map(|f| (f.bit_offset() + f.bit_width() / 2, f))
+        .collect::<HashMap<_, _>>();
+
+    (0..32)
+        .rev()
+        .map(|bit| {
+            if let Some(field) = fields.get(&bit) {
+                field.name.to_owned()
             } else {
-                flds.push((31, 0));
+                String::new()
             }
+        })
+        .collect::<Vec<_>>()
+}
 
-            writeln!(writer, "<tr>").ok();
-            for (from, to) in flds {
-                let desc = if from != to {
-                    format!("{} - {}", from, to)
-                } else {
-                    format!("{}", from)
-                };
-                let span = from - to + 1;
-                writeln!(writer, r#"<td colspan="{}">{}</td>"#, span, desc).ok();
-            }
-            writeln!(writer, "</tr>").ok();
+fn field_spans(register: &MaybeArray<RegisterInfo>) -> Vec<Value> {
+    let mut fields = register
+        .fields()
+        .map(|f| (f.bit_offset() + f.bit_width() - 1, f.bit_offset()))
+        .rev()
+        .collect::<Vec<(_, _)>>();
 
-            // bits
-            writeln!(writer, "<tr>").ok();
-            for bit in (0..32).rev() {
-                writeln!(writer, r#"<td>{}</td>"#, bit).ok();
-            }
-            writeln!(writer, "</tr>").ok();
+    let mut at = 0;
+    for i in (0..fields.len()).rev() {
+        let (from, to) = fields[i];
 
-            writeln!(writer, "<table>").ok();
-
-            // describe fields
-            for field in register.fields() {
-                let desc = match &field.description {
-                    Some(desc) => desc.to_string(),
-                    None => EMPTY.clone(),
-                };
-                let access = match &field.access {
-                    Some(access) => match access {
-                        svd_parser::svd::Access::ReadOnly => "R".to_string(),
-                        svd_parser::svd::Access::ReadWrite => "RW".to_string(),
-                        svd_parser::svd::Access::ReadWriteOnce => "RWO".to_string(),
-                        svd_parser::svd::Access::WriteOnce => "WO".to_string(),
-                        svd_parser::svd::Access::WriteOnly => "W".to_string(),
-                    },
-                    None => "-".to_string(),
-                };
-                writeln!(
-                    writer,
-                    r#"<p><b>{}</b> <i>{}</i> {}</p>"#,
-                    field.name, access, desc
-                )
-                .ok();
-            }
-            writeln!(writer, "</br>").ok();
+        if to > at {
+            fields.insert(i + 1, (at + (to - at) - 1, at));
         }
 
-        writeln!(writer, "</br></br></br>").ok();
+        at = from + 1;
     }
-    writeln!(writer, "</body></html>").ok();
+
+    if fields.len() > 0 {
+        let (f, _) = fields[0];
+        if f < 31 {
+            fields.insert(0, (31, f + 1));
+        }
+    } else {
+        fields.push((31, 0));
+    }
+
+    fields
+        .iter()
+        .map(|(from, to)| {
+            context! {
+                span => from - to + 1,
+                text => if from == to {
+                    format!("{}", from)
+                } else {
+                    format!("{} - {}", from, to)
+                },
+            }
+        })
+        .collect::<Vec<_>>()
+}
+
+fn field_descriptions(register: &MaybeArray<RegisterInfo>) -> Vec<Value> {
+    register
+        .fields()
+        .map(|f| {
+            let desc = match &f.description {
+                Some(d) => d.to_owned(),
+                None => String::new(),
+            };
+
+            let access = match &f.access {
+                Some(access) => match access {
+                    Access::ReadOnly => "R",
+                    Access::ReadWrite => "RW",
+                    Access::ReadWriteOnce => "RWO",
+                    Access::WriteOnce => "WO",
+                    Access::WriteOnly => "W",
+                },
+                None => "-",
+            };
+
+            context! {
+                name        => f.name.clone(),
+                description => desc,
+                access      => access,
+            }
+        })
+        .collect::<Vec<_>>()
+}
+
+fn write_html(source: &String, path: &PathBuf) -> Result<()> {
+    eprintln!("Writing: {}", path.display());
+
+    let mut file = File::create(path)?;
+    file.write_all(source.as_bytes())?;
+
+    Ok(())
 }
